970cbb91cd15196d5de8be1bf8044d6940785351
Vanek <Starkiller1960@mail.ru>
2024-09-18 23:23:57 +0700

    small fixes

--- src/widget.py
+++ src/widget.py
@@ -15,9 +15,9 @@ def mask_account_card(card: str) -> str:
 
 print(mask_account_card("Visa Platinum 7000792289606364"))
 
+
 def get_date(user_date: Union[str]) -> str:
     """Функция получения даты в определенном формате и возвращения в формате ДД.ММ.ГГГГ"""
     date_format = datetime.strptime(user_date, "%Y-%m-%dT%H:%M:%S.%f")
     new_date = date_format.strftime("%d.%m.%Y")
     return new_date
-
