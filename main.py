ae59d0ed03d6fe6c357b1cd9e706c580a0e8cc63
Vanek <Starkiller1960@mail.ru>
2024-09-18 23:34:49 +0700

    sintax fixes

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ from src.masks import get_mask_card_number
 
 print(get_mask_card_number("1234567891234567"))
 
-from src.widget import  get_date
+from src.widget import get_date
 
 print(get_date("2024-03-11T02:26:18.671407"))
